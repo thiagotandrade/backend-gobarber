4a6db8cf3b0af5723cdea5892ccc027a4c8404b3
Thiago Andrade <thiagotales@gmail.com>
2020-08-28 14:08:26 -0300

    Add typeorm config and connection, create entities

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
-import express from 'express';
+import 'reflect-metadata';
 
+import express from 'express';
 import routes from './routes';
+
+import './database';
 
 const app = express();
 
@@ -8,5 +11,6 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
+  // eslint-disable-next-line no-console
   console.log('🚀 Server started!');
 });
