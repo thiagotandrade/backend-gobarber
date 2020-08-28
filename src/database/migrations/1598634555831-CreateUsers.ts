328728d49debfc6c773d86fc7c46cb3c4c085f13
Thiago Andrade <thiagotales@gmail.com>
2020-08-28 19:45:11 -0300

    Create user and appointment FK

--- src/database/migrations/1598634555831-CreateUsers.ts
+++ src/database/migrations/1598634555831-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1598547115727
-  implements MigrationInterface {
+export default class CreateUsers1598634555831 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,12 +14,16 @@ export default class CreateAppointments1598547115727
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -38,6 +41,6 @@ export default class CreateAppointments1598547115727
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
