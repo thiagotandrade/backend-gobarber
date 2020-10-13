bc839d31d559e7e9002ec10d8ed63378465ba18b
thiagotandrade <thiagotales@gmail.com>
2020-10-13 08:41:22 -0300

    List all providers

---
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,40 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProviders: ListProvidersService;
+
+describe('ListProviders', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    listProviders = new ListProvidersService(fakeUsersRepository);
+  });
+
+  it('should be able to list the providers', async () => {
+    const user1 = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123123',
+    });
+
+    const user2 = await fakeUsersRepository.create({
+      name: 'John Trê',
+      email: 'johntre@example.com',
+      password: '123123',
+    });
+
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'John Quá',
+      email: 'johnqua@example.com',
+      password: '123123',
+    });
+
+    const users = await listProviders.execute({
+      user_id: loggedUser.id,
+    });
+
+    expect(users).toEqual([user1, user2]);
+  });
+});
